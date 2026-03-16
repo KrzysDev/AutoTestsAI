02775dbff918a76aa78986927c544ca033f2640b
KrzysDev <krzysiekk.sokolowski@gmail.com>
2026-03-16 23:21:17 +0100

    feat: added rate l;imiting to limit API usage

--- backend/app/api/routers/ai_router.py
+++ backend/app/api/routers/ai_router.py
@@ -1,10 +1,14 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter, HTTPException, Request
 import ollama
 from app.services.ai_service import AiService
+from datetime import datetime
+from collections import defaultdict
 
 router = APIRouter()
 
 ai_service = AiService()
+
+last_requests_cloud = defaultdict(lambda: datetime.min)
 
 @router.post("/v1/rag/ask/local")
 def ask_local(text: str):
@@ -18,7 +22,16 @@ def ask_local(text: str):
         raise HTTPException(status_code=500, detail="Nieoczekiwany blad modelu lokalnego.")
 
 @router.post("/v1/rag/ask/cloud")
-def ask_cloud(text: str):
+def ask_cloud(text: str, request: Request):
+    client_ip = request.client.host
+    now = datetime.now()
+    delta = (now - last_requests_cloud[client_ip]).total_seconds()
+    
+    if delta < 3:
+        raise HTTPException(status_code=429, detail="Zbyt duża liczba zapytań. Spróbuj ponownie za moment.")
+    
+    last_requests_cloud[client_ip] = now
+
     if not text.strip():
         raise HTTPException(status_code=400, detail="Tekst wejściowy nie może być pusty")
     try:
