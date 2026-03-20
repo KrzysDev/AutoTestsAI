e719de77d97f6da0ad781c92399ae5374dd57700
KrzysDev <krzysiekk.sokolowski@gmail.com>
2026-03-20 22:01:39 +0100

    feat: added prompt to describe what local model has to extract from photo

--- scripts/data_extractor.py
+++ scripts/data_extractor.py
@@ -10,7 +10,30 @@ class DataExtractor:
         self.ai_service = AiService()
 
     def extract_data(self, file_path):
-        prompt = "Co znajduje się na tym zdjęciu? Opisz to krótko."
+        prompt = """
+        #ZADANIE
+        Musisz wyeksportować dane z przesłanych ci zdjęć. Są to zdjęcia podreczników z języka angielskiego.
+        Gdy zobaczysz słówka, zapisz je jako listę. Gdy zobaczysz definicję gramatyki (np. czas Present Simple z języka angielskiego) zapisz jej definicję, przykłady oraz 
+        wszystko to co znajdziesz w podręczniku.
+
+        #WYMAGANIA
+        - Odpowiedź musi być w języku polskim,
+        - Odpowiedź musi być w formacie JSON, dokładnie takim, jaki zostanie przedstawiony poniżej. Nie dodawaj niczego przed oraz po JSONie.
+        {
+            "id":        " ", #unikalny identyfikator 
+            "section":   " ", #nazwa sekcji np. vocabulary lub grammar
+            "language":  " ", #język                  
+            "level":     " ", #poziom np. B2, B1, C2, C1 itp.                    
+            
+            "metadata" : {
+                "subject" : "",  #temat np - środowisko, życie w mieście, technologia, zawody i przyszłość, oświata itp. (Temat często podany jest na samej górze strony w podręczniku)    
+                "content": " ", #zawartość chunka - dla gramatyki (grammar) bedzie to część jakiejś definicji (np. Present Simple używamy gdy...) natomiast dla słówek (vocabulary) - fragment listy słówek.
+                #Dla każdego słówka z listy słówek postaraj się zawrzeć po przykladzie użycia go w zdaniu.
+            }
+        }
+
+        
+        """
         response = self.ai_service.ask_local_with_photo(prompt, file_path)
         return response.get("message", response)
 
@@ -20,6 +43,8 @@ class DataExtractor:
 def main():
     root = tk.Tk()
     root.withdraw()
+
+    extractor = DataExtractor()
 
     file_path = filedialog.askopenfilename(
         title="Wybierz zdjęcie",
@@ -39,13 +64,12 @@ def main():
         print(f"Błąd inicjalizacji AiService: {e}")
         return
         
-    prompt = "Co znajduje się na tym zdjęciu? Opisz to krótko."
     print("Wysyłanie zapytania do modelu...")
     
     try:
-        response = ai_service.ask_local(prompt, file_path)
+        response = extractor.extract_data(file_path)
         print("\nOdpowiedź modelu:")
-        print(response.get("message", response))
+        print(response)
     except Exception as e:
         print(f"Błąd podczas odpytywania modelu: {e}")
 
