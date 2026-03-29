fd7cc794cee387ae11cdf16be0a7ad78da5c9718
KrzysDev <krzysiekk.sokolowski@gmail.com>
2026-03-29 17:58:37 +0200

    feat: added ocr service which returns chunked text extracted from photo

--- backend/app/services/data_extraction_service.py
+++ backend/app/services/data_extraction_service.py
@@ -1,15 +1,5 @@
-import json
-import hashlib
-from typing import Literal
-from enum import Enum
-from backend.app.services.ai_service import AiService
-from backend.app.services.chunking_service import ChunkingService
-from backend.app.models.schemas import Chunk, ChunkMetadata
-import sys
+import enum
 
-
-
-from backend.app.models.prompts import SystemPrompts as prompts
 
 class ExtractionType(Enum):
     cloud = 1
@@ -21,11 +11,8 @@ class DataExtractionService:
         self.chunking_service = ChunkingService()
         self.existing_subjects = []
 
-    def extract_data(self, section: Literal["vocab", "gram"], language: Literal["en", "de"], level: Literal["A1", "A2", "B1", "B2", "C1", "C2"], photo_path: str = None, extraction_type: ExtractionType = ExtractionType.cloud):
-        if section == "vocab":
-            return self.__extract_vocab(text, language, level, extraction_type)
-        elif section == "gram":
-            return self.__extract_gram(text, language, level, extraction_type)
+    def extract_data(self):
+        pass
 
     """Used to extract vocabulary from OCR text"""
     def __extract_vocab(self, text: str,language: Literal["en", "de"], level: Literal["A1", "A2", "B1", "B2", "C1", "C2"], extraction_type: ExtractionType = ExtractionType.cloud) -> list[Chunk]:
--- backend/app/services/ocr_service.py
+++ backend/app/services/ocr_service.py
@@ -1,9 +1,33 @@
 import easyocr
+import image_slicer
+import os
 
 class OCRService:
     def __init__(self):
         self.reader = easyocr.Reader(['en', 'de', 'pl'], gpu=True)
+        self.count = 0
 
     def extract_text(self, photo_path: str) -> str:
-        result = self.reader.readtext(photo_path)
-        return "\n".join([item[1] for item in result])
+        image_slicer.slice_image(photo_path, cols=4, rows=4, output_dir=rf"C:\Users\USER\Desktop\Ai Test Generator Dataset-20260321T142317Z-1-001\vocabulary_dataset\tiles_{self.count}")
+
+        tiles = []
+        for filename in os.listdir(rf"C:\Users\USER\Desktop\Ai Test Generator Dataset-20260321T142317Z-1-001\vocabulary_dataset\tiles_{self.count}"):
+            if filename.endswith(".jpg") or filename.endswith(".png"):
+                with open(rf"C:\Users\USER\Desktop\Ai Test Generator Dataset-20260321T142317Z-1-001\vocabulary_dataset\tiles_{self.count}\{filename}", "rb") as f:
+                    tiles.append(f.read())
+        
+        result = []
+
+        id_count = 0
+
+        for tile in tiles:
+            extracted_text = self.reader.readtext(tile)
+            result.append({
+                "id": id_count,
+                "text": " ".join([item[1] for item in extracted_text])
+            })
+
+            id_count += 1
+
+        self.count += 1
+        return result
