343dadc21162da8665316639e5b28d3a9bfb1eb1
KrzysDev <krzysiekk.sokolowski@gmail.com>
2026-04-09 23:42:28 +0200

    chore: removed unused imports and deleted wrong logic from generator service. going to make it once once again...

--- backend/app/models/schemas.py
+++ backend/app/models/schemas.py
@@ -64,77 +64,8 @@ class TestGeneratorRequest(BaseModel):
     group_count: int = 2
 
 
-#section in transformed prompt. Section explained to AI model
-class PromptTestSection(BaseModel):
-    section_type: str
-    subject : str
-    amount : int
-
 #section format in test. Used to print it on pdf and as model output
 class GeneratedTestSection(BaseModel):
     instruction: str
     body: str
-
-class FirstLayerRules(BaseModel):
-    task_info: str
-    level_info: str
-    target_age_info: str
-    sections_info: str
-    total_tasks_info: str
-    output_rule_info: str
-
-class TransformedPrompt(BaseModel):
-    task: str 
-    level : Literal["A1", "A2", "B1", "B2", "C1", "C2"]
-    target_age: str
-    sections: list[PromptTestSection]
-    total_tasks: int
-
-class RetrievalInstructions(BaseModel):
-    grammar_instructions: list[RetrivedChunk] = Field(alias="Grammar_Instructions")
-    exercise_instructions: list[RetrivedChunk] = Field(alias="Exercise_Instructions")
-    
-    model_config = ConfigDict(populate_by_name=True)  
-
-class RetrievalData(BaseModel):
-    instructions: RetrievalInstructions = Field(alias="Instructions")
-    
-    model_config = ConfigDict(populate_by_name=True)  
-
-class GenerationPrompt(BaseModel):
-    twoja_rola: str = Field(
-        default="Jesteś ekspertem w tworzeniu materiałów dydaktycznych do nauki języków obcych. Twoim zadaniem jest stworzenie jednego zadania na podstawie dostarczonych danych z retrivalu.",
-        alias="twoja rola"
-    )
-    zasady: str = Field(
-        default="W polu 'dane z retrival' podane są przykładowe zadania na których MUSISZ się wzorować tworząc nowe podobnego rodzaju. Skup sie na poziomie jaki reprezentuja i jak są skoonstruowane. Poniżej znajdziesz też instrukcje w jaki sposób krok po kroku stworzyć tego typu zadanie. Musisz zwrócić wyłącznie JSON w wymaganym formacie. Nic więcej poza nim. Nie dodawaj nic przed ani po jsonie w tym znaków markdown takich jak ```json lub ```",
-        alias="zasady"
-    )
-    zasada_braku_powtarzalnosci: str = Field(
-        default="absolutnie, nie wolno ci pod żadnym pozorem, robić takich samych typów zadań które się już pojawiły, CHYBA ŻE NAUCZYCIEL POWIEDZIAL INACZEJ.",
-        alias="zasada_braku_powtarzalnosci"
-    )
-
-    szczegóły_pól: str = Field(
-        default="level - poziom językowy CEFR (A1, A2, B1...C2), age_group - docelowa grupa wiekowa (kids, teens, adults), task_type - typ zadania (np. vocabulary, grammar, reading etc. musisz wybrac jeden), topic - temat zadania (np. present simple, present contionous, reading etc. musisz wybrac jeden), amount - ilość wystąpień / ile zadan musisz w tej liscie tego typu stworzyć (wybierz dowolnie, chyba że została podana przez nauczyciela)",
-        alias="szczegóły_pół"
-    )
-    prosba_nauczyciela: str = Field(alias="prosba_nauczyciela")
-    dane_z_retrieval: RetrievalData = Field(alias="dane z retrival")
-    wymagany_format_json: dict = Field(
-        default={
-            "Question": {
-                "content": {
-                    "instruction": "",
-                    "body": ""
-                }
-            }
-        },
-        alias="wymagany format json"
-    )
-
-    model_config = ConfigDict(populate_by_name=True)
-    
-
-
 
---
+++ backend/app/services/prompt_parser_service.py
@@ -0,0 +1,8 @@
+from backend.app.services.ai_service import AiService
+
+class PromptParserService:
+    def __init__():
+        pass
+
+    def parse_prompt(self, text: str):
+        pass
