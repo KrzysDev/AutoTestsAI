c936799cdab59f5ee1ad1d1d21f090bca2eec36f
KrzysDev <krzysiekk.sokolowski@gmail.com>
2026-04-19 00:19:45 +0200

    feat: cors middleware fix

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,11 +1,24 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
 from backend.app.api.routers.search_router import router as search_router
 from backend.app.api.routers.test_generation_router import router as test_generation_router
 from backend.app.api.routers.ai_router import router as ai_router
 from backend.app.api.routers.json_test_converting_router import router as json_test_converting_router
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # During development, allowing all. We can restrict this later.
+    allow_credentials=False,
+
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(search_router)
+
 app.include_router(test_generation_router)
 app.include_router(ai_router)
 app.include_router(json_test_converting_router)
