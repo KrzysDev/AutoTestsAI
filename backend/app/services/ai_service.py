a620145037e2382b231c0784dd4b611300d0311d
KrzysDev <krzysiekk.sokolowski@gmail.com>
2026-04-21 21:28:16 +0200

    feat: added testing mode with ollama cloud to save money

--- backend/app/services/ai_service.py
+++ backend/app/services/ai_service.py
@@ -1,29 +1,48 @@
 import requests
 import os
 import dotenv
+from ollama import Client
 
 dotenv.load_dotenv()
 
 class AiService:
     def __init__(self):
-        pass
+        self.ollama_client = Client(
+            host="https://ollama.com",
+            headers={'Authorization': 'Bearer ' + str(os.getenv('OLLAMA_API_KEY'))}
+        )
+        self.model = "gemini-3-flash-preview"
 
     def ask(self, text: str):
-        response = requests.post(
-            url="https://openrouter.ai/api/v1/chat/completions",
-            headers={
-                "Authorization": f"Bearer {os.getenv('OPENROUTER_API_KEY')}",
-            },
-            json={
-                "model": "google/gemini-3.1-flash-lite-preview",
-                "messages": [
+        is_testing = os.getenv("AI_TESTING", "false").lower() == "true"
+
+        if is_testing:
+            response = self.ollama_client.chat(
+                model=self.model,
+                messages=[
                     {
                         "role": "user",
                         "content": text
                     }
                 ]
-            }
-        )
+            )
+            return response["message"]["content"]
+        else:
+            response = requests.post(
+                url="https://openrouter.ai/api/v1/chat/completions",
+                headers={
+                    "Authorization": f"Bearer {os.getenv('OPENROUTER_API_KEY')}",
+                },
+                json={
+                    "model": self.model,
+                    "messages": [
+                        {
+                            "role": "user",
+                            "content": text
+                        }
+                    ]
+                }
+            )
 
-        data = response.json()
-        return data["choices"][0]["message"]["content"]
+            data = response.json()
+            return data["choices"][0]["message"]["content"]
